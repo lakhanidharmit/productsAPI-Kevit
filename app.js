73b004ef7a7290ba39a49dcf1668ca67f4a28545
Dharmit Lakhani <lakhanidharmit@yahoo.com>
2022-12-14 08:17:24 +0530

    created application layout

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const serverConfig = require('./configs/server.config')
+const dbConfig = require('./configs/db.config');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+db.on("error", ()=>{
+    console.log("#### Error while connecting to mongoDB ####");
+});
+db.once("open",()=>{
+    console.log("#### Connected to mongoDB ####");
+});
+
+// require('./routes/products.routes')(app);
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`#### connected to server at port no.: ${serverConfig.PORT} ####`);
+})
